fde36424615a48c84b490bf314ce09c7cfaca15a
mcharkin <mcharkin@gmail.com>
2015-03-21 13:19:34 -0700

    added parse integration

--- mobile/Slug/Slug/AppDelegate.swift
+++ mobile/Slug/Slug/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
+    
+    
+    Parse.setApplicationId("jrYIeSLHv75tMFMrKFmMEx7vgIUSv9mJh2WfzATh", clientKey: "ee0fxObMg8eRDN8aNKsKhL1ZeWEBrXyZe98q8r0a")
+    
+    PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
+    
     return true
   }
 
--- mobile/Slug/Slug/ViewController.swift
+++ mobile/Slug/Slug/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let testObj = PFObject(className: "TestObject")
+    testObj.setValue("bar", forKey: "foo")
+    testObj.saveInBackgroundWithBlock(nil)
   }
 
   override func didReceiveMemoryWarning() {
