33a3c360b0f9f2648f013b70037d43b621444dbb
Charimon <acharkin@gmail.com>
2015-03-22 16:40:57 -0700

    added parse integration

---
+++ web/source/javascripts/parse.js
@@ -0,0 +1,12 @@
+Parse.initialize("7GExJ8EPPiEb8oYZwbEsfIBXeCwK4XavXhOeJvqL", "ocAubh6BhhpGvGNgciSv5iOb5rs6lnRuerZyYVFj");
+
+document.getElementById("signUp").addEventListener("click", function(){
+  var inputValue = document.getElementById("emailInput").value
+
+  var EmailObject = Parse.Object.extend("EmailObject");
+  var emailObject = new EmailObject();
+  emailObject.save({email: inputValue}).then(function(object) {
+    alert("yay! thanks");
+  });
+
+});
