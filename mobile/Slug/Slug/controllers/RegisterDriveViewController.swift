f90c653d69c12c4280ff78f2724c9c310e3a4916
Charimon <acharkin@gmail.com>
2015-03-21 18:57:41 -0700

    added ride setup

--- mobile/Slug/Slug/controllers/RegisterDriveViewController.swift
+++ mobile/Slug/Slug/controllers/RegisterDriveViewController.swift
@@ -7,33 +7,57 @@
 //
 
 import UIKit
+import Parse
 
 class RegisterDriveViewController: UIViewController {
+
+  var maxSpaces: Int?
+  var departure: NSDate?
+  
   @IBOutlet var seats: [UIButton]!
+  @IBOutlet var departureTimes: [UIButton]!
   
   @IBAction func selectSeats(sender: UIButton) {
-    for seat in seats {
+    for seat in self.seats {
       seat.selected = false
     }
     sender.selected = true
+    self.maxSpaces = sender.tag
   }
 
   @IBAction func depart5Minutes(sender: UIButton) {
-    
+    for departureButton in self.departureTimes {
+      departureButton.selected = false
+    }
+    self.departure = NSDate().dateByAddingTimeInterval(60*5)
+    sender.selected = true;
   }
   @IBAction func depart15Minutes(sender: UIButton) {
-    
+    for departureButton in self.departureTimes {
+      departureButton.selected = false
+    }
+    self.departure = NSDate().dateByAddingTimeInterval(60*15)
+    sender.selected = true;
   }
   @IBAction func depart30Minutes(sender: UIButton) {
-    
+    for departureButton in self.departureTimes {
+      departureButton.selected = false
+    }
+    self.departure = NSDate().dateByAddingTimeInterval(60*15)
+    sender.selected = true;
   }
   
-  @IBAction func unwind(segue: UIStoryboardSegue) {
-    self.view.backgroundColor = UIColor.redColor()
-    self.dismissViewControllerAnimated(true, completion: nil)
-  }
   @IBAction func done(sender: UIButton) {
-    println("hERE")
-    self.dismissViewControllerAnimated(true, completion: nil)
+    if let maxSpaces = self.maxSpaces {
+      if let departure = self.departure {
+        let slugUser = SlugUser(parseUser: PFUser.currentUser())
+        
+        let ride = Ride(driver: slugUser, maxSpaces: maxSpaces, departure: departure)
+        ride.parseObj.saveInBackgroundWithBlock(nil)
+        
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+      }
+    }
   }
 }
