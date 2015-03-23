d50a096bbc37b6aa1af4f013ff5eda6004172e58
mcharkin <mcharkin@gmail.com>
2015-03-22 23:17:30 -0700

    updated with relative paths

--- javascripts/parse.js
+++ javascripts/parse.js
@@ -1,12 +1,1 @@
-Parse.initialize("7GExJ8EPPiEb8oYZwbEsfIBXeCwK4XavXhOeJvqL", "ocAubh6BhhpGvGNgciSv5iOb5rs6lnRuerZyYVFj");
-
-document.getElementById("signUp").addEventListener("click", function(){
-  var inputValue = document.getElementById("emailInput").value
-
-  var EmailObject = Parse.Object.extend("EmailObject");
-  var emailObject = new EmailObject();
-  emailObject.save({email: inputValue}).then(function(object) {
-    document.getElementById("thankYouText").style.display = 'block'
-  });
-
-});
+Parse.initialize("7GExJ8EPPiEb8oYZwbEsfIBXeCwK4XavXhOeJvqL","ocAubh6BhhpGvGNgciSv5iOb5rs6lnRuerZyYVFj"),document.getElementById("signUp").addEventListener("click",function(){var e=document.getElementById("emailInput").value,t=Parse.Object.extend("EmailObject"),n=new t;n.save({email:e}).then(function(e){document.getElementById("thankYouText").style.display="block"})});
